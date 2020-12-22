905a8492d85a38ef42481947229cfc8e4286d72f
Aluno manhã <alunomanha@senai.ti>
2020-12-22 12:06:27 -0300

    atualizando projetos

---
+++ js/listacompra.js
@@ -0,0 +1,15 @@
+ver lista = document.querySelector("#list");
+console.log(lista);
+//console.log(lista)
+
+function additem() {
+    /**pegando valor digitado nos inputs
+     */
+    //valor do item
+    var item = document.querySelector("new-item");
+
+    //console.log(item)
+    var item = document.querySelector("new-item")
+    .nodeValue;
+    console.log(item)
+}
